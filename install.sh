9a9a771740ede47aa5f46276678decc820aa7985
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-03-28 23:25:24 -0100

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #install all soft
-sudo pacman -S waybar swaybg river-classsic foot fuzzel fastfetch eza tree bat dysk yazi ncdu pcmanfm
+sudo pacman -S waybar swaybg river-classic foot fuzzel fastfetch eza tree bat dysk yazi ncdu
 # copy all files to .config in home directory
 cp -rv ./.config/* ~/.config/
