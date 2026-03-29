6b4956956f5e7e6f0776946eb1cb4e8658db6caf
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-03-29 21:15:24 +0000

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #install all soft
-# video sound seat ?
-sudo usermod -aG video $USER
-sudo pacman -S ttf-jetbrains-mono-nerd waybar river-classic foot fuzzel fastfetch eza tree bat dysk yazi ncdu
+# river uses seat
+sudo usermod -aG seat $USER
+# I like this font
+sudo pacman -S ttf-jetbrains-mono-nerd waybar river-classic foot fuzzel fastfetch eza tree bat dysk yazi ncdu lynx
 # copy all files to .config in home directory
 cp -rv ./.config/* ~/.config/
 cp ./.config/bash/.* ~/
