00e39573fc4c9725479a7ff6271fddefd3252362
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-03-29 20:06:37 +0000

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #install all soft
+sudo usermod -aG seat $USER
 sudo pacman -S ttf-jetbrains-mono-nerd waybar river-classic foot fuzzel fastfetch eza tree bat dysk yazi ncdu
 # copy all files to .config in home directory
 cp -rv ./.config/* ~/.config/
