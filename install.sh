799cd89ee018fafd733027b18f9bb211304e2195
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-03-30 14:41:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #install all soft
 # river uses seat
 sudo usermod -aG seat $USER
+sudo systemctl enable seatd
 # I like this font
 sudo pacman -S ttf-jetbrains-mono-nerd waybar river-classic foot fuzzel fastfetch eza tree bat dysk yazi ncdu lynx
 # copy all files to .config in home directory
