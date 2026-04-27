2819bf40fe2494e9b98e8dba3f65faaa5710bec7
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-04-27 22:32:32 +0000

    Update install_alpine.sh

--- install_alpine.sh
+++ install_alpine.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-
 #install all soft
 # river uses seat
 apk add seatd
 rc-update add seatd
 rc-service seatd start 
 doas adduser $USER seat
+doas setup-wayland-base
 # I like this font
 doas apk add font-jetbrains-mono-nerd ncurses waybar river-classic river-classic-doc foot fuzzel fastfetch eza tree bat dysk yazi ncdu lynx
+# enable grapic card
+# missing ccode
 # copy all files to .config in home directory
 cp -rv ./.config/* ~/.config/
 rm ~/.config/fastfetch/*
