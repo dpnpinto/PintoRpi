2bed21ea1f8c997357159a232425ff8c95b63d89
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-04-27 10:48:16 +0000

    Update downldmem.sh

--- downldmem.sh
+++ downldmem.sh
@@ -1,17 +1,25 @@
-Create the swap file: (e.g., 4GB)
-sudo fallocate -l 4G /swapfile
-Set permissions:
-sudo chmod 600 /swapfile
-Setup as swap space:
-sudo mkswap /swapfile
-Enable the swap:
-sudo swapon /swapfile
-Make it permanent: Add to /etc/fstab.
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-Verify:
-sudo swapon --show or free -h
+#!/bin/bash
+# Create the swap file: (e.g., 4GB)
+sudo fallocate -l 512M /swapfile
 
-# Example command in U-Boot console
-setenv bootargs ${bootargs} zswap.enabled=1
-saveenv
-boot
+# Set permissions:
+sudo chmod 600 /swapfile
+
+# Setup as swap space:
+sudo mkswap /swapfile
+
+# Enable the swap
+sudo swapon /swapfile
+
+# Make it permanent: Add to /etc/fstab.
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Verify
+sudo swapon --show
+free -h
+
+# Command in U-Boot console "boot.txt"
+# setenv bootargs ${bootargs} zswap.enabled=1
+# saveenv
+# boot
+sudo sed -i 's/bootargs/bootargs zswap.enabled=1/g' /boot/boot.txt
