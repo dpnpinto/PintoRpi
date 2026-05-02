a295b2bda7c0b219544cb74d22139f7ca400ffa5
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-05-02 15:13:29 +0000

    Update create_swap.sh

--- zswap/create_swap.sh
+++ zswap/create_swap.sh
@@ -17,9 +17,3 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 # Verify
 sudo swapon --show
 free -h
-
-# Enable zswap with command in U-Boot console "boot.txt"
-# setenv bootargs ${bootargs} zswap.enabled=1
-# saveenv
-# boot
-sudo sed -i 's/bootargs/bootargs zswap.enabled=1/g' /boot/boot.txt
