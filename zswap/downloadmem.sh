ada66a2043ee08773c30eebdc60ca6b8c4b71c62
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-05-03 12:36:39 +0000

    Delete zswap/downloadmem.sh

--- zswap/downloadmem.sh
+++
@@ -1,8 +0,0 @@
-# Check current parameters
-sudo grep -r . /sys/module/zswap/parameters/
-# enbale the service to personalizae zswap
-sudo cp zswap-config.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable zswap-config.service
-# Check stats
-sudo grep -r . /sys/kernel/debug/zswap/
