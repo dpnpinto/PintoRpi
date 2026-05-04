32abf655aa7d255312b19d94f8fb9c15dd7a3c91
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-05-04 09:45:02 +0000

    Update download_mem.sh

--- zswap/download_mem.sh
+++ zswap/download_mem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Check current parameters
 sudo grep -r . /sys/module/zswap/parameters/
-# enbale the service to personalizae zswap
+# enable the service to personalizae zswap
 sudo cp zswap-config.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable zswap-config.service
